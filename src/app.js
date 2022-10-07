9d6a431bca3aba8799948747efe54941a33cdbf8
Makson <maksonsantoss4@gmail.com>
2022-10-07 15:40:51 -0300

    feat: Entrega finalizada passando em todos os testes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import "dotenv/config";
+import { routeCategories, routeProducts } from "./routes/routes";
+import { startDatabase } from "./database";
 
 const app = express();
-
 app.use(express.json());
+app.use("", routeCategories);
+app.use("", routeProducts);
 
-export default app.listen(3333, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+export default app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+  startDatabase();
 });
---
+++ src/controller/categories/listCategories.controller.js
@@ -0,0 +1,10 @@
+import { listCategoriesService } from "../../services/categories/listCategories.service";
+
+export async function listCategoriesController(req, res) {
+  try {
+    const categories = await listCategoriesService();
+    return res.status(200).json(categories);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+}
